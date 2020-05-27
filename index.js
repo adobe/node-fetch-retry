147fd31805d48366f7b46579ed2dde6d8c2bbc32
Deirdra Strangio <deirdra.strangio@gmail.com>
2020-05-27 12:00:16 -0600

    NUI-492 Support Windows 10+ (#12)
    * Updated eslint and fixed complaints
    * Added windows support to package.json
    * Fixed badges in readme
    * Added current code coverage method and format
    * Simplified package.json script commands
    * Added Beautify to scripts

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /*
-Copyright 2020 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
-
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 'use strict';
 
 const fetch = require('node-fetch-npm');
@@ -71,9 +70,9 @@ function retryInit(options={}) {
             retryInitialDelay: retryOptions.retryInitialDelay || DEFAULT_INITIAL_WAIT,
             retryBackoff: retryOptions.retryBackoff || DEFAULT_BACKOFF,
             retryOnHttpResponse: ((typeof retryOptions.retryOnHttpResponse === 'function') && retryOptions.retryOnHttpResponse) ||
-                ((response) => { return response.status >= 500 }),
+                ((response) => { return response.status >= 500; }),
             socketTimeout: socketTimeoutValue
-        }
+        };
     }
     return false;
 }
@@ -137,7 +136,7 @@ function checkParameters(retryOptions) {
 /**
  * Fetch retry that wraps around `node-fetch` library
  * @param {Options} options options for fetch request RetryOptions for retries or `false` if no do not want to perform retries
- * @returns {Object} json repsonse of calling fetch
+ * @returns {Object} json response of calling fetch
  */
 module.exports = async function (url, options) {
     options = options || {};
@@ -166,8 +165,8 @@ module.exports = async function (url, options) {
 
             retryOptions.retryInitialDelay *= retryOptions.retryBackoff; // update retry interval
             const waitTime = retryDelay(retryOptions);
-            setTimeout(() => { wrappedFetch() }, waitTime);
-        }
+            setTimeout(() => { wrappedFetch(); }, waitTime);
+        };
         wrappedFetch();
-    })
-}
+    });
+};
