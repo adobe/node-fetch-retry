24d20a624b340121c087ae7fd1acc8482f6a6cf2
Johannes Schindelin <dscho@github.com>
2022-01-24 22:46:01 +0100

    FEATURE-RELEASE: Add Typescript declarations (#42)
    This makes it possible to use `@adobe/node-fetch-retry` in Typescript
    projects.
    
    It is _also_ used by recent ESLint versions for extra type-checking
    goodness, where e.g. missing returns or incorrect return types are
    made immediately obvious already while editing code in an IDE.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+import {RequestInfo, RequestInit, Response} from 'node-fetch'
+
+export interface RequestInitWithRetry extends RequestInit {
+  retryOptions?: {
+    retryMaxDuration?: number
+    retryInitialDelay?: number
+    retryBackoff?: number
+    retryOnHttpError?: (error: Error) => boolean
+    retryOnHttpResponse?: (response: Response) => boolean
+    socketTimeout?: number
+    forceSocketTimeout?: boolean
+  }
+}
+
+declare function fetch(
+  url: RequestInfo,
+  init?: RequestInitWithRetry
+): Promise<Response>
+
+export default fetch
