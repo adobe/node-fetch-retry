df371a691172aee76e7d43e1dbc28f2e868389c8
Tom <20443472+NiewView@users.noreply.github.com>
2022-02-09 14:33:41 +0100

    fix types to allow disabling retry

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,17 @@
 import {RequestInfo, RequestInit, Response} from 'node-fetch'
 
+export interface RetryOptions{
+  retryMaxDuration?: number
+  retryInitialDelay?: number
+  retryBackoff?: number
+  retryOnHttpError?: (error: Error) => boolean
+  retryOnHttpResponse?: (response: Response) => boolean
+  socketTimeout?: number
+  forceSocketTimeout?: boolean
+}
+
 export interface RequestInitWithRetry extends RequestInit {
-  retryOptions?: {
-    retryMaxDuration?: number
-    retryInitialDelay?: number
-    retryBackoff?: number
-    retryOnHttpError?: (error: Error) => boolean
-    retryOnHttpResponse?: (response: Response) => boolean
-    socketTimeout?: number
-    forceSocketTimeout?: boolean
-  }
+  retryOptions?: RetryOptions | false
 }
 
 declare function fetch(
